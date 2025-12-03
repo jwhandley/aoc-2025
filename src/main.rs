cab26e0861c6d37ee658f834a92fce507c1ea0a0
John Handley <jwhandley17@gmail.com>
2025-12-03 20:14:48 +0000

    Add option to run all days

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,49 @@
 use anyhow::Context;
 use aoc_2025::solution_for;
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
 struct Args {
-    day: u32,
+    #[command(subcommand)]
+    command: Action,
+}
+
+#[derive(Subcommand)]
+enum Action {
+    Day { day: u32 },
+    All,
 }
 
 fn main() -> Result<(), anyhow::Error> {
     let args = Args::parse();
-    let input = std::fs::read_to_string(format!("./inputs/{:02}.txt", args.day))
-        .with_context(|| format!("Unable to find input for day {}", args.day))?;
-    let (part1, part2) = solution_for(args.day)?;
+    match args.command {
+        Action::Day { day } => solve_day(day),
+        Action::All => {
+            for day in 1..=25 {
+                if let Ok((part1, part2)) = solution_for(day) {
+                    let input = std::fs::read_to_string(format!("./inputs/{:02}.txt", day))
+                        .with_context(|| format!("Unable to find input for day {}", day))?;
+
+                    let now = std::time::Instant::now();
+                    let r1 = part1(&input)?;
+                    println!("Part 1: {r1}");
+
+                    let r2 = part2(&input)?;
+                    println!("Part 2: {r2}");
+
+                    println!("Solved in {:?}", now.elapsed());
+                }
+            }
+
+            Ok(())
+        }
+    }
+}
+
+fn solve_day(day: u32) -> Result<(), anyhow::Error> {
+    let input = std::fs::read_to_string(format!("./inputs/{:02}.txt", day))
+        .with_context(|| format!("Unable to find input for day {}", day))?;
+    let (part1, part2) = solution_for(day)?;
 
     let now = std::time::Instant::now();
     let r1 = part1(&input)?;
