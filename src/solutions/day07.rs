80bf7abb72606651e8482dbf62b8577bc17b27c0
John Handley <jwhandley17@gmail.com>
2025-12-07 16:13:13 +0000

    Use DP instead of DFS/caching

--- src/solutions/day07.rs
+++ src/solutions/day07.rs
@@ -1,84 +1,44 @@
-use glam::IVec2;
-use std::collections::{HashMap, HashSet, VecDeque};
-
 pub fn part1(input: &str) -> Result<String, anyhow::Error> {
-    let (start, height, splitters) = parse_input(input);
+    let lines: Vec<&str> = input.lines().collect();
+    let (first, rest) = lines.split_first().unwrap();
 
-    let mut q = VecDeque::new();
-    let mut seen = HashSet::new();
-    q.push_back(start);
-    let mut total = 0;
-
-    while let Some(next) = q.pop_front() {
-        if next.x >= height as i32 || !seen.insert(next) {
-            continue;
-        }
-
-        if splitters.contains(&(next + IVec2::X)) {
-            total += 1;
-            q.push_back(next + IVec2::new(1, -1));
-            q.push_back(next + IVec2::new(1, 1));
-        } else {
-            q.push_back(next + IVec2::X);
+    let mut cols: Vec<bool> = first.chars().map(|c| c == 'S').collect();
+    let mut count = 0;
+    for row in rest.iter() {
+        for (i, c) in row.char_indices() {
+            if cols[i] && c == '^' {
+                cols[i - 1] = true;
+                cols[i] = false;
+                cols[i + 1] = true;
+                count += 1;
+            }
         }
     }
 
-    Ok(total.to_string())
+    Ok(count.to_string())
 }
 
 pub fn part2(input: &str) -> Result<String, anyhow::Error> {
-    let (start, height, splitters) = parse_input(input);
+    let lines: Vec<&str> = input.lines().collect();
+    let (first, rest) = lines.split_first().unwrap();
 
-    let mut cache = HashMap::new();
-    let total = solve(start, &splitters, height, &mut cache);
-    Ok(total.to_string())
-}
+    let mut cols: Vec<usize> = first
+        .chars()
+        .map(|c| if c == 'S' { 1 } else { 0 })
+        .collect();
 
-fn parse_input(input: &str) -> (IVec2, i32, HashSet<IVec2>) {
-    let mut height = 0;
-    let mut start = IVec2::ZERO;
-    let mut splitters = HashSet::new();
-    for (r, line) in input.lines().enumerate() {
-        for (c, val) in line.char_indices() {
-            if val == 'S' {
-                start = IVec2::new(r as i32, c as i32);
-            }
-
-            if val == '^' {
-                splitters.insert(IVec2::new(r as i32, c as i32));
+    for row in rest.iter() {
+        for (i, c) in row.char_indices() {
+            if cols[i] > 0 && c == '^' {
+                cols[i - 1] += cols[i];
+                cols[i + 1] += cols[i];
+                cols[i] = 0;
             }
         }
-        height += 1;
     }
 
-    (start, height, splitters)
-}
-
-fn solve(
-    pos: IVec2,
-    splitters: &HashSet<IVec2>,
-    height: i32,
-    cache: &mut HashMap<IVec2, usize>,
-) -> usize {
-    if let Some(&n) = cache.get(&pos) {
-        return n;
-    }
-
-    if pos.x >= height {
-        return 1;
-    }
-
-    let result = if splitters.contains(&pos) {
-        let left = solve(pos + IVec2::new(1, -1), splitters, height, cache);
-        let right = solve(pos + IVec2::new(1, 1), splitters, height, cache);
-
-        left + right
-    } else {
-        solve(pos + IVec2::X, splitters, height, cache)
-    };
-
-    cache.insert(pos, result);
-    result
+    let total: usize = cols.iter().sum();
+    Ok(total.to_string())
 }
 
 #[cfg(test)]
